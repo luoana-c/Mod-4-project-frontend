7dfb4382a5c80f5477af4c01e9d6f537b37a2443
Adham <admuaq@gmail.com>
2018-10-03 12:15:36 +0100

    added scrollable menu feature

--- src/containers/Canvas.js
+++ src/containers/Canvas.js
@@ -7,18 +7,6 @@ import DraggableTemplate from '../components/DraggableTemplate';
 const Canvas = (props) => {
   return (
     <div className='canvas'>
-      {props.iconsOnCanvas && props.iconsOnCanvas.map((icon, index) =>
-        <DraggableIcon
-          key={`canvas-icon-${index}`}
-          handleDrag={event => props.handleDrag(icon, event)} 
-          handleResize={event => props.handleResize(icon, event)} 
-          icon={icon}
-          canvasDimensions={props.canvasDimensions}
-          removeFromCanvas={props.removeFromCanvas}
-          selectIcon={props.selectIcon}
-          position={icon.pos}
-          // size={icon.size}
-        />)}
       {props.templatesOnCanvas && props.templatesOnCanvas.map((template, index) =>
         <DraggableTemplate
           key={`canvas-template-${index}`}
@@ -30,6 +18,18 @@ const Canvas = (props) => {
           selectIcon={props.selectIcon}
           position={template.pos}
           // size={template.size}
+        />)}
+      {props.iconsOnCanvas && props.iconsOnCanvas.map((icon, index) =>
+        <DraggableIcon
+          key={`canvas-icon-${index}`}
+          handleDrag={event => props.handleDrag(icon, event)} 
+          handleResize={event => props.handleResize(icon, event)} 
+          icon={icon}
+          canvasDimensions={props.canvasDimensions}
+          removeFromCanvas={props.removeFromCanvas}
+          selectIcon={props.selectIcon}
+          position={icon.pos}
+          // size={icon.size}
         />)}
     </div>
   )
